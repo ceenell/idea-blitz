f6fe1fa4d8eb48e731dd6fd311113c3c9cda3352
collnell <cnell@usgs.gov>
2022-07-06 12:53:52 -0500

    set up subdirectory

---
+++ bloom-bubbles/scratch.R
@@ -0,0 +1,17 @@
+## Exploration for a cluster bubble chart data visualization
+
+
+# prelims -----------------------------------------------------------------
+
+library(tidyverse)
+
+## Objectives:
+### Understand community data
+### look at differences in magnitude and composition through time
+
+## Inspiration
+### fact sheet - https://pubs.usgs.gov/fs/2022/3011/fs20223011.pdf
+
+# data --------------------------------------------------------------------
+
+
