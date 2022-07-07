b21c95b0e99746e72118a73b07252a7041e24fd7
Platt <lplatt@usgs.gov>
2022-07-07 15:07:40 -0500

    add fetch targets to get field visit data from NWIS

--- hydrotech-highlight/_targets.R
+++ hydrotech-highlight/_targets.R
@@ -2,11 +2,37 @@ library(targets)
 
 options(tidyverse.quiet = TRUE)
 tar_option_set(packages = c(
+  'dataRetrieval',
+  'sf',
   'tidyverse'
 ))
 
+query_startDate <- '2022-01-01'
+query_endDate <- '2022-06-30'
+
 ##### Download data #####
 p1_targets <- list(
+
+  # Identify which sites have had a field visit across the U.S.
+  tar_target(p1_field_visit_sites,
+             purrr::map(sprintf("%02d", 1:21), function(huc_code) {
+               whatNWISsites(huc = huc_code,
+                             service='meas',
+                             startDate = query_startDate,
+                             endDate = query_endDate,
+                             siteType = 'ST') %>%
+                 pull(site_no) %>% unique()
+             }) %>% reduce(c) %>% unique()),
+
+  # Download the actual field measurment data for these sites
+  tar_target(p1_field_visit_data, readNWISmeas(
+    siteNumbers = p1_field_visit_sites,
+    startDate = query_startDate,
+    endDate = query_endDate)),
+
+  # Create a spatial object for the site locations
+  tar_target(p1_field_visit_sites_sf, readNWISsite(p1_field_visit_sites) %>%
+               st_as_sf(coords = c('dec_long_va', 'dec_lat_va'), crs = 4326))
 
 )
 
