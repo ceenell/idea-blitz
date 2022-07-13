2131437ccdcd81b38e0a89d4179b6093cf20eb5c
Platt <lplatt@usgs.gov>
2022-07-13 13:50:00 -0500

    add documentation and clean up functions

--- hydrotech-highlight/fxns.R
+++ hydrotech-highlight/fxns.R
@@ -81,23 +81,52 @@ identify_most_visited_sites <- function(field_visit_data, top_n = 5) {
     select(site_no, n_total_visits = n)
 }
 
+#' @title Download population data from the U.S. Census Bureau
+#' @description This function uses the `tidycensus` package to download
+#' resident population data using the 'place' geographies from the Census
+#' Bureau. Note that the API call used here was not returning data for
+#' Puerto Rico, so we excluded it in the subsequent functions that use the
+#' population data.
+#'
+#' @details To use this, you will need to have a local API key setup. Follow the
+#' instructions at https://walker-data.com/tidycensus/articles/basic-usage.html
+#' for requesting a key. You will receive the email fast, but note that you may
+#' need to wait a few minutes before successfully clicking on the activate key
+#' URL. Once the key is activated, run `census_api_key("YOUR KEY", install = TRUE)`
+#' and then you should be able to use this function.
+#'
+#' @param state_cd vector of character abbreviations for states to pull place data
+#'
+#' @return an `sf` object with polygons for the places, plus `GEOID` (US Census
+#' Bureau defined place code), `NAME` (plain text place name), `variable` (will
+#' only say "POP" for population), and `value` (the resident population count) columns.
+#'
+
 pull_population_data <- function(state_cd) {
-  # https://walker-data.com/tidycensus/articles/basic-usage.html
-
-  # Signed up for a key (takes < 1 min to get one), then I had to
-  # wait 2 minutes after receiving the email before I could click
-  # the activate URL and successfully activate my key. Then, I ran
-  # `census_api_key("YOUR KEY", install = TRUE)` to store my key.
-
   get_estimates(
-    # Can't get anything newer than 2019 to work
     'place',
+    year = 2019, # Can't get anything newer than 2019 to work
     state = state_cd,
     variable = "POP",
     geometry = TRUE)
 }
-# Chose 50k as minimum because that means that there is at least one city per state
-# because Burlington, VT is the lowest "most populated" state city.
+
+#' @title Clean the population data to use in later steps
+#' @description This function applies a filter to keep only places of a certain
+#' resident size and converts the polygons into centroids.
+#'
+#' @param all_population_data the `sf` object returned from `pull_population_data()`
+#' @param min_value single numeric value indicating the minimum population a
+#' place should have to remain in the dataset. Note that the default is 50k
+#' residents, which means that some states (such as Vermont, where the most
+#' populated city is Burlington with just under 43k) will not have any places
+#' included in the final dataset.
+#'
+#' @return an `sf` object with points for the places, plus `GEOID` (US Census
+#' Bureau defined place code), `NAME` (plain text place name), and `population`
+#' (the resident population count) columns.
+#'
+
 prepare_population_data <- function(all_population_data, min_value = 50*10^3) {
   all_population_data %>%
     select(-variable, population = value) %>%
@@ -105,6 +134,22 @@ prepare_population_data <- function(all_population_data, min_value = 50*10^3) {
     st_centroid() %>%
     st_transform(crs = 4326)
 }
+
+#' @title For each site, identify the closest populated place
+#' @description This function identifies the nearest place for each gage and
+#' how far that place is in kilometers and miles. It also filters out sites
+#' in Puerto Rico due to the limitation noted in `pull_population_data()` with
+#' pulling place population data for Puerto Rico.
+#'
+#' @param sites_sf an `sf` object created using NWIS site metadata
+#' @param places_sf the `sf` object returned from `prepare_population_data()`
+#'
+#' @return an `sf` object with points for gages, all site metadata columns
+#' returned by `dataRetrieval::readNWISsite()`, plus `nearest_place` (US Census
+#' Bureau plain text name for a location), `nearest_place_distance_km` (distance
+#' in kilometers from the gage to the `nearest_place`), and `nearest_place_distance_mi`
+#' (distance in miles from the gage to the `nearest_place`).
+#'
 
 identify_nearest_place <- function(sites_sf, places_sf) {
   distances_mat <- st_distance(sites_sf, places_sf)
@@ -118,11 +163,21 @@ identify_nearest_place <- function(sites_sf, places_sf) {
     filter(state_cd != '72')
   return(nearest_info)
 }
-# field_visit_data <- tar_read(p1_field_visit_data)
+
+#' @title For each site, tally and then average annual visits
+#' @description This function calculates an average annual number
+#' of visits for each site. Note that visits where `measurement_dt`
+#' is `NA` are dropped entirely here. For 2012-06-30 to 2022-06-30,
+#' 1% of the field visits (~ 8,500 visits) had `NA` in the
+#' `measurement_dt` field and were dropped.
+#'
+#' @param field_visit_data the data.frame object returned by `dataRetrieval::readNWISmeas()`
+#'
+#' @return a data.frame with two columns, `site_no` and `avg_annual_visits`
+#'
+
 calculate_avg_annual_visits <- function(field_visit_data) {
   field_visit_data %>%
-    # We lose a bunch of sites here because they have an NA
-    # for the measurement_dt
     mutate(year = lubridate::year(measurement_dt)) %>%
     group_by(site_no, year) %>%
     tally() %>%
@@ -130,12 +185,22 @@ calculate_avg_annual_visits <- function(field_visit_data) {
     summarize(avg_annual_visits = mean(n, na.rm=TRUE))
 }
 
+#' @title Categorize each field visit record into one of the 24 hours in a day
+#' @description This function takes the `measurement_tm` column from the field
+#' visit measurement records and categorizes into a single hour of the day between
+#' 1 and 24 by rounding up (e.g "09:12" is the 10th hour of the day, while "09:00"
+#' is the last value of the 9th hour of the day).
+#'
+#' @param field_visit_data the data.frame object returned by `dataRetrieval::readNWISmeas()`
+#'
+#' @return a data.frame with the same columns as `field_visit_data` plus `time_of_day`,
+#' representing an integer hour of the day between 1 and 24
+#'
+
 categorize_visit_into_time_of_day <- function(field_visit_data) {
   field_visit_data %>%
-    separate(measurement_tm, c('hours', 'seconds'), sep = ':') %>%
-    mutate(time_of_day = ceiling(as.numeric(hours) + as.numeric(seconds)/60)) %>%
-    dplyr::select(-hours, -seconds) %>%
-    # Making assumption that "work hours" are between 7 AM and 6 PM local time
-    mutate(is_afterwork = time_of_day > 18 | time_of_day < 7) %>%
+    separate(measurement_tm, c('hours', 'minutes'), sep = ':') %>%
+    mutate(time_of_day = ceiling(as.numeric(hours) + as.numeric(minutes)/60)) %>%
+    dplyr::select(-hours, -minutes) %>%
     filter(!is.na(time_of_day) & time_of_day != 0)
 }
